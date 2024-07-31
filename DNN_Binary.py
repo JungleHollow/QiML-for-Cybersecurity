52974e9dd9f51b6e86a9f100dc34b5c343e5995d
Manuel Saavedra Sepúlveda <79433265+JungleHollow@users.noreply.github.com>
2024-07-31 17:38:31 +0800

    Update DNN_Binary.py

--- DNN_Binary.py
+++ DNN_Binary.py
@@ -6,22 +6,46 @@ import pandas as pd
 import numpy as np
 np.random.seed(1337)
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import save_model, load_model, Sequential
 from tensorflow.keras.constraints import MaxNorm
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.backend import clear_session
+from tensorflow.data import Dataset
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, confusion_matrix, precision_recall_curve
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, Normalizer, StandardScaler
 import pennylane as qml
-from pennylane.templates import AngleEmbedding, StronglyEntanglingLayers
-from pennylane.operation import Tensor
-from pennylane.optimize import NesterovMomentumOptimizer
+from pennylane.templates import AngleEmbedding, BasicEntanglerLayers
 
 tf.keras.backend.set_floatx("float64")
+
+# ==== Initialise parameters as globals outside __name__ == __main__ so that spawned Windows processes can see them ====
+
+global TRAINING_PATH, VALIDATION_PATH, TESTING_PATH
+TRAINING_PATH = r"E:\HonoursThesis\Code\Datasets\Cleaned Datasets\UNSW_NB15_training-set"
+VALIDATION_PATH = r"E:\HonoursThesis\Code\Datasets\Cleaned Datasets\UNSW_NB15_validation-set"
+TESTING_PATH = r"E:\HonoursThesis\Code\Datasets\Cleaned Datasets\UNSW_NB15_testing-set"
+# TRAINING_PATH = r"E:\HonoursThesis\Code\Datasets\Cleaned Datasets\CICIDS17_training-set"
+# VALIDATION_PATH = r"E:\HonoursThesis\Code\Datasets\Cleaned Datasets\CICIDS17_validation-set"
+# TESTING_PATH = r"E:\HonoursThesis\Code\Datasets\Cleaned Datasets\CICIDS17_testing-set"
+
+global DATASET, N_LAYERS, DROPOUT, LEARNING_RATE, BATCH_SIZE, N_EPOCHS, THRESHOLD, SAVEPATH
+DATASET = "UNSW"
+# DATASET = "CICIDS17"
+N_LAYERS = 3
+DROPOUT = 0.1
+LEARNING_RATE = 0.01
+BATCH_SIZE = 64
+N_EPOCHS = 1000
+THRESHOLD = 0.5
+SAVEPATH = rf"E:\HonoursThesis\Code\models_binary\DNN_{N_LAYERS}-layer_{N_EPOCHS}-epochs_{DATASET}.keras"
+
+global N_QUBITS, Q_SAVEPATH
+N_QUBITS = 12
+Q_SAVEPATH = rf"E:\HonoursThesis\Code\models_binary\DQiNN_{N_LAYERS}-layer_{N_QUBITS}-qubit_{N_EPOCHS}-epochs_{DATASET}.keras"
 
 
 # ==== Reset the keras session (trying to work around memory leak issues) ====
@@ -31,71 +55,36 @@ def reset_keras():
 
 
 # ==== Read in the stored data appropriately to use for model training and evaluation ====
-def read_data(trainpath, testpath):
-    log_cols = ["dur", "sbytes", "dbytes", "sload", "dload", "spkts", "stcpb", "dtcpb", "sjit", "djit"]
+def read_data(trainpath, valpath, testpath):
+    train_ds = Dataset.load(trainpath)
+    val_ds = Dataset.load(valpath)
 
-    train_frame = pd.read_csv(trainpath)
-    train_frame["service"] = train_frame["service"].apply(lambda x: "None" if x == "-" else x)
-    train_frame[log_cols].apply(np.log1p)  # Apply logarithm transformations to skewed features
-
-    test_frame = pd.read_csv(testpath)
-    test_frame["service"] = test_frame["service"].apply(lambda x: "None" if x == "-" else x)
-    test_frame[log_cols].apply(np.log1p)
-
-    train_numeric = train_frame.drop(columns=["id", "label"], axis=1).select_dtypes(include=["number"]).columns
-    test_numeric = test_frame.drop(columns=["id", "label"], axis=1).select_dtypes(include=["number"]).columns
-
-    # Standardize the numeric variables in each dataframe
-    scaler = StandardScaler()
-    train_frame[train_numeric] = scaler.fit_transform(train_frame[train_numeric], train_frame["label"])
-    test_frame[test_numeric] = scaler.transform(test_frame[test_numeric])
-
-    del train_numeric, test_numeric
-
-    # Onehot encode the categorical variables in each dataframe
-    for col in ["proto", "service", "state"]:
-        ohe = OneHotEncoder(handle_unknown="ignore")
-
-        train_cols = ohe.fit_transform(train_frame[col].values.reshape(-1, 1))
-        test_cols = ohe.transform(test_frame[col].values.reshape(-1, 1))
-
-        train_df = pd.DataFrame(train_cols.todense(), columns=[col + "_" + str(i) for i in ohe.categories_[0]])
-        test_df = pd.DataFrame(test_cols.todense(), columns=[col + "_" + str(i) for i in ohe.categories_[0]])
-
-        train_frame = pd.concat([train_frame.drop(col, axis=1), train_df], axis=1)
-        test_frame = pd.concat([test_frame.drop(col, axis=1), test_df], axis=1)
-
-    # Sample one in every 50 packets randomly (reasonable training times without much loss in predictive power)
-    train_frame, discarded_train = train_test_split(train_frame, test_size=0.95, random_state=1337)
-    test_frame, discarded_test = train_test_split(test_frame, test_size=0.95, random_state=1337)
-
-    del discarded_train, discarded_test
-
-    # Sort by id to maintain the temporal structure of the data
-    train_frame.sort_values(by=["id"], inplace=True)
-    test_frame.sort_values(by=["id"], inplace=True)
-
-    # Split each dataframe into their respective input and target variables for use in model training and evaluation
-    y_train = train_frame["label"]
-    x_train = train_frame.drop(["id", "attack_cat", "label"], axis=1).to_numpy().astype("float32")
+    test_ds = Dataset.load(testpath)
+    test_frame = pd.read_csv(f"{testpath}.csv")
 
     y_test = test_frame["label"]
-    x_test = test_frame.drop(["id", "attack_cat", "label"], axis=1).to_numpy().astype("float32")
 
-    del train_frame, test_frame
+    del test_frame
 
-    return x_train, y_train, x_test, y_test
+    return train_ds, val_ds, test_ds, y_test
+
+
+# ==== Workaround used to make the tensorflow csv datasets compatible with keras model functions ====
+def dataset_fix(features, labels):
+    return tf.stack(list(features.values()), axis=1), labels
 
 
 # ==== Model classes with their appropriate implementations and configurations ====
 class DNN:
-    def __init__(self, trainpath, testpath, savepath, loadpath=None, layers=1, dropout=0.01,
+    def __init__(self, trainpath, valpath, testpath, savepath, loadpath=None, layers=1, dropout=0.01,
                  learning_rate=0.01, batch_size=32, epochs=100, threshold=0.5):
-        self.training_set, self.training_labels, self.testing_set, self.testing_labels = self.read_data(trainpath, testpath)
+        self.training_set, self.validation_set, self.testing_set, self.testing_labels = self.read_data(trainpath, valpath, testpath)
         gc.collect()
 
         self.save_path = savepath
-        self.chk_path = f"./models_binary/checkpoints/{self.save_path.split('/')[-1]}"
+        tmp_name = self.save_path.split("\\")[-1]
+        self.chk_path = rf"E:\HonoursThesis\Code\models_binary\checkpoints\{tmp_name}"
+        del tmp_name
 
         self.batch_size = batch_size
         self.prediction_threshold = threshold
@@ -119,6 +108,7 @@ class DNN:
                 self.model.add(Dense(128, activation="relu"))
             self.model.add(Dense(1, activation="sigmoid"))
 
+        self.early_stop = EarlyStopping(monitor="val_loss", patience=30, mode="min")
         self.checkpointing = ModelCheckpoint(filepath=self.chk_path, save_best_only=True, monitor="val_loss",
                                              mode="min")
 
@@ -133,19 +123,19 @@ class DNN:
         self.recall = 0.0
         self.f1_score = 0.0
 
-    def read_data(self, trainpath, testpath):
-        return read_data(trainpath, testpath)
+    def read_data(self, trainpath, valpath, testpath):
+        return read_data(trainpath, valpath, testpath)
 
     def train_model(self):
         self.model.fit(
-            self.training_set, self.training_labels, validation_split=0.1, epochs=self.epochs,
-            batch_size=self.batch_size, callbacks=[self.checkpointing]
+            self.training_set.map(dataset_fix), validation_data=self.validation_set.map(dataset_fix), epochs=self.epochs,
+            callbacks=[self.checkpointing, self.early_stop]
         )
         gc.collect()
 
     def evaluate_model(self):
         best_model = load_model(self.chk_path)
-        prediction_proba = best_model.predict(self.testing_set, batch_size=self.batch_size)
+        prediction_proba = best_model.predict(self.testing_set.map(dataset_fix))
 
         prediction_proba = prediction_proba.reshape(prediction_proba.shape[0],)
 
@@ -157,9 +147,6 @@ class DNN:
 
         gc.collect()
 
-        conf_mat = confusion_matrix(self.testing_labels, prediction_classes)
-        print(conf_mat)
-
         loss_fn = BinaryCrossentropy()
         self.loss = loss_fn(self.testing_labels, prediction_proba)
         self.accuracy = accuracy_score(self.testing_labels, prediction_classes)
@@ -169,6 +156,7 @@ class DNN:
         self.f1_score = f1_score(self.testing_labels, prediction_classes)
 
         print(f"==== MODEL EVALUATION COMPLETE ====\n\n- Loss: {self.loss}\n- Accuracy: {self.accuracy}\n- Precision: {self.precision}\n- Recall: {self.recall}\n- F1 Score: {self.f1_score}\n")
+        print(best_model.summary())
 
     def save(self):
         self.model.save(self.save_path)
@@ -177,13 +165,15 @@ class DNN:
 
 # Loading and saving of DQiNN models is done with weights instead of full models to work around a layer type issue
 class DQiNN:
-    def __init__(self, trainpath, testpath, savepath, loadpath=None, layers=1, dropout=0.01,
+    def __init__(self, trainpath, valpath, testpath, savepath, loadpath=None, layers=1, dropout=0.01,
                  learning_rate=0.01, batch_size=32, epochs=100, threshold=0.5, n_qubits=16):
-        self.training_set, self.training_labels, self.testing_set, self.testing_labels = self.read_data(trainpath, testpath)
+        self.training_set, self.validation_set, self.testing_set, self.testing_labels = self.read_data(trainpath, valpath, testpath)
         gc.collect()
 
         self.save_path = savepath
-        self.chk_path = f"./models_binary/checkpoints/{self.save_path.split('/')[-1]}"
+        tmp_name = self.save_path.split("\\")[-1]
+        self.chk_path = rf"E:\HonoursThesis\Code\models_binary\checkpoints\{tmp_name}"
+        del tmp_name
 
         self.prediction_threshold = threshold
         self.batch_size = batch_size
@@ -195,29 +185,33 @@ class DQiNN:
 
         self.weight_shapes = {"weights": (self.n_layers, self.n_qubits)}
 
-        @qml.qnode(self.dev)
+        @qml.qnode(self.dev, diff_method="adjoint")
         def qnode(inputs, weights):
             qml.AngleEmbedding(inputs, wires=self.dev.wires)
             qml.BasicEntanglerLayers(weights, wires=self.dev.wires)
             return [qml.expval(qml.PauliZ(wires=i)) for i in self.dev.wires]
 
         self.qlayer = qml.qnn.KerasLayer(qnode, self.weight_shapes, output_dim=self.n_qubits)
+        self.qlayer_2 = qml.qnn.KerasLayer(qnode, self.weight_shapes, output_dim=self.n_qubits)
+        self.qlayer_3 = qml.qnn.KerasLayer(qnode, self.weight_shapes, output_dim=self.n_qubits)
+        self.qlayer_4 = qml.qnn.KerasLayer(qnode, self.weight_shapes, output_dim=self.n_qubits)
+        self.qlayer_5 = qml.qnn.KerasLayer(qnode, self.weight_shapes, output_dim=self.n_qubits)
 
         self.model = Sequential()
         self.model.add(Dense(self.n_qubits, activation="relu"))
         self.model.add(self.qlayer)
         if self.n_layers >= 2:
             self.model.add(Dropout(self.dropout))
-            self.model.add(self.qlayer)
+            self.model.add(self.qlayer_2)
         if self.n_layers >= 3:
             self.model.add(Dropout(self.dropout))
-            self.model.add(self.qlayer)
+            self.model.add(self.qlayer_3)
         if self.n_layers >= 4:
             self.model.add(Dropout(self.dropout))
-            self.model.add(self.qlayer)
+            self.model.add(self.qlayer_4)
         if self.n_layers >= 5:
             self.model.add(Dropout(self.dropout))
-            self.model.add(self.qlayer)
+            self.model.add(self.qlayer_5)
         self.model.add(Dense(1, activation="sigmoid"))
 
         self.lr = learning_rate
@@ -226,9 +220,10 @@ class DQiNN:
         self.model.compile(loss="binary_crossentropy", optimizer=self.opt, metrics=["accuracy"])
 
         if loadpath:
-            self.model.fit(self.training_set, self.training_labels, batch_size=1, steps_per_epoch=1, epochs=1, verbose=0)   # Needed to allow weight loading
+            self.model.fit(self.training_set.map(dataset_fix), steps_per_epoch=1, epochs=1, verbose=0)   # Needed to allow weight loading
             self.model.load_weights(loadpath)
 
+        self.early_stop = EarlyStopping(monitor="val_loss", patience=30, mode="min")
         self.checkpointing = ModelCheckpoint(filepath=self.chk_path, save_best_only=True, monitor="val_loss",
                                              mode="min", save_weights_only=True)
 
@@ -238,13 +233,13 @@ class DQiNN:
         self.recall = 0.0
         self.f1_score = 0.0
 
-    def read_data(self, trainpath, testpath):
-        return read_data(trainpath, testpath)
+    def read_data(self, trainpath, valpath, testpath):
+        return read_data(trainpath, valpath, testpath)
 
     def train_model(self):
         self.model.fit(
-            self.training_set, self.training_labels, validation_split=0.1, epochs=self.epochs,
-            batch_size=self.batch_size, callbacks=[self.checkpointing]
+            self.training_set.map(dataset_fix), validation_data=self.validation_set.map(dataset_fix), epochs=self.epochs,
+            callbacks=[self.checkpointing, self.early_stop]
         )
         gc.collect()
 
@@ -254,22 +249,23 @@ class DQiNN:
         best_model.add(self.qlayer)
         if self.n_layers >= 2:
             best_model.add(Dropout(self.dropout))
-            best_model.add(self.qlayer)
+            best_model.add(self.qlayer_2)
         if self.n_layers >= 3:
             best_model.add(Dropout(self.dropout))
-            best_model.add(self.qlayer)
+            best_model.add(self.qlayer_3)
         if self.n_layers >= 4:
             best_model.add(Dropout(self.dropout))
-            best_model.add(self.qlayer)
+            best_model.add(self.qlayer_4)
         if self.n_layers >= 5:
             best_model.add(Dropout(self.dropout))
-            best_model.add(self.qlayer)
+            best_model.add(self.qlayer_5)
         best_model.add(Dense(1, activation="sigmoid"))
+
         best_model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-        best_model.fit(self.training_set, self.training_labels, batch_size=1, steps_per_epoch=1, epochs=1, verbose=0)  # Needed to allow weight loading
+        best_model.fit(self.training_set.map(dataset_fix), steps_per_epoch=1, epochs=1, verbose=0)  # Needed to allow weight loading
         best_model.load_weights(self.chk_path)
 
-        prediction_proba = best_model.predict(self.testing_set, batch_size=self.batch_size)
+        prediction_proba = best_model.predict(self.testing_set.map(dataset_fix))
 
         prediction_proba = prediction_proba.reshape(prediction_proba.shape[0],)
 
@@ -290,6 +286,7 @@ class DQiNN:
         self.f1_score = f1_score(self.testing_labels, prediction_classes)
 
         print(f"==== MODEL EVALUATION COMPLETE ====\n\n- Loss: {self.loss}\n- Accuracy: {self.accuracy}\n- Precision: {self.precision}\n- Recall: {self.recall}\n- F1 Score: {self.f1_score}\n")
+        print(best_model.summary())
 
     def save(self):
         self.model.save_weights(self.save_path)
@@ -299,12 +296,12 @@ class DQiNN:
 # ==== Dummy functions to enable multiprocessing trick (workaround for keras model.predict() memory leak) ====
 def init_dnn():
     if os.path.isfile(SAVEPATH):
-        model = DNN(TRAINING_PATH, TESTING_PATH, SAVEPATH, loadpath=SAVEPATH, layers=N_LAYERS,
+        model = DNN(TRAINING_PATH, VALIDATION_PATH, TESTING_PATH, SAVEPATH, loadpath=SAVEPATH, layers=N_LAYERS,
                     dropout=DROPOUT, learning_rate=LEARNING_RATE, batch_size=BATCH_SIZE, epochs=N_EPOCHS,
                     threshold=THRESHOLD)
         model.evaluate_model()
     else:
-        model = DNN(TRAINING_PATH, TESTING_PATH, SAVEPATH, layers=N_LAYERS, dropout=DROPOUT,
+        model = DNN(TRAINING_PATH, VALIDATION_PATH, TESTING_PATH, SAVEPATH, layers=N_LAYERS, dropout=DROPOUT,
                     learning_rate=LEARNING_RATE, batch_size=BATCH_SIZE, epochs=N_EPOCHS, threshold=THRESHOLD)
         model.train_model()
         model.save()
@@ -313,12 +310,12 @@ def init_dnn():
 
 def init_dqinn():
     if os.path.isfile(Q_SAVEPATH):
-        my_q_model = DQiNN(TRAINING_PATH, TESTING_PATH, Q_SAVEPATH, loadpath=Q_SAVEPATH,
+        my_q_model = DQiNN(TRAINING_PATH, VALIDATION_PATH, TESTING_PATH, Q_SAVEPATH, loadpath=Q_SAVEPATH,
                            layers=N_LAYERS, dropout=DROPOUT, learning_rate=LEARNING_RATE, batch_size=BATCH_SIZE,
                            epochs=N_EPOCHS, threshold=THRESHOLD, n_qubits=N_QUBITS)
         my_q_model.evaluate_model()
     else:
-        my_q_model = DQiNN(TRAINING_PATH, TESTING_PATH, Q_SAVEPATH, layers=N_LAYERS, dropout=DROPOUT,
+        my_q_model = DQiNN(TRAINING_PATH, VALIDATION_PATH, TESTING_PATH, Q_SAVEPATH, layers=N_LAYERS, dropout=DROPOUT,
                            learning_rate=LEARNING_RATE, batch_size=BATCH_SIZE, epochs=N_EPOCHS, threshold=THRESHOLD,
                            n_qubits=N_QUBITS)
         my_q_model.train_model()
@@ -330,24 +327,9 @@ def init_dqinn():
 if __name__ == "__main__":
     reset_keras()
 
-    TRAINING_PATH = "./Datasets/UNSW_NB15/UNSW_NB15_training-set.csv"
-    TESTING_PATH = "./Datasets/UNSW_NB15/UNSW_NB15_testing-set.csv"
-
-    DATASET = "UNSW"
-    N_LAYERS = 3
-    DROPOUT = 0.1
-    LEARNING_RATE = 0.01
-    BATCH_SIZE = 64
-    N_EPOCHS = 1000
-    THRESHOLD = 0.5
-    SAVEPATH = f"./models_binary/DNN_{N_LAYERS}-layer_{N_EPOCHS}-epochs_{DATASET}.keras"
-
     # p = multiprocessing.Process(target=init_dnn)
     # p.start()
     # p.join()
-
-    N_QUBITS = 8
-    Q_SAVEPATH = f"./models_binary/DQiNN_{N_LAYERS}-layer_{N_QUBITS}-qubit_{N_EPOCHS}-epochs_{DATASET}.keras"
 
     p_q = multiprocessing.Process(target=init_dqinn)
     p_q.start()
