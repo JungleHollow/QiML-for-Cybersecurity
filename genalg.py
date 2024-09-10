c447cfe5935ee846fdfa5f80769fd381af53bcdd
Manuel Saavedra Sepúlveda <79433265+JungleHollow@users.noreply.github.com>
2024-09-10 18:33:19 +0800

    Update genalg.py

--- genalg.py
+++ genalg.py
@@ -9,6 +9,7 @@ import pennylane as qml
 import os
 
 
+# Test matrix used to ensure the GA worked before progressing to model evaluations
 # target = np.array(
 #     [[0., -300, -0.78401152, 500, -0.2533131],
 #      [-0.05186097, 0., -0.44479764, -0.27236475, -0.0633621],
@@ -17,6 +18,7 @@ import os
 #      [-0.0808, -0.78747657, -0.20279267, -0.96499505, 0.]])
 
 
+# Utility function that creates the pennylane CNOT gates as well as the model's standardised save name
 def create_gates(entanglement_matrix: np.ndarray) -> (list[qml.CNOT], str):
     gates = []
     save_pairs = []
@@ -32,13 +34,14 @@ def create_gates(entanglement_matrix: np.ndarray) -> (list[qml.CNOT], str):
     return gates, save_name
 
 
+# Trains and evaluates a model with the given adjacency matrix, using the F1 score as the fitness score
 def fit_func(chromo: cr.Chromo) -> float:
     # score = np.sum(np.multiply(target, chromo.matrix))
     # return float(score)
     dent.reset_keras()  # Fix memory leak issues with repeated keras model fitting and evaluating
     gates, save_name = create_gates(chromo.matrix)
 
-    save_path = rf"C:\Users\WornA\OneDrive\Desktop\Uni Work\Honours Thesis\Code\models_binary\DQiNN_{save_name}_{dent.N_QUBITS}-qubit_{dent.N_LAYERS}-layer_{dent.DATASET}.keras"
+    save_path = f"./Code/models_binary/DQiNN_{save_name}_{dent.N_QUBITS}-qubit_{dent.N_LAYERS}-layer_{dent.DATASET}.keras"
 
     if not os.path.isfile(save_path):
         my_q_model = dent.DQiNN(dent.TRAINING_PATH, dent.VALIDATION_PATH, dent.TESTING_PATH, save_path, layers=dent.N_LAYERS, dropout=dent.DROPOUT,
@@ -64,13 +67,13 @@ def fit_func(chromo: cr.Chromo) -> float:
 
 class GA(object):
     def __init__(self, dims: int, dom: cr.DatType, low_b: float, upp_b: float, pop_size_pairs: int, prob_xover: float, prob_mut: float, fitness: Callable[[cr.Chromo], float]) -> None:
-        self.dims = dims
-        self.dom = dom
-        self.low_b = low_b
-        self.upp_b = upp_b
+        self.dims = dims  # In this case, the number of quantum wires in the circuits being tested
+        self.dom = dom   # The numerical domain of the matrix entries (should always be cr.DatType.INTEGER)
+        self.low_b = low_b  # The lower bound of acceptable matrix values (should always be 0)
+        self.upp_b = upp_b  # The upper bound of acceptable matrix values (should always be 1)
         self.pop_size = pop_size_pairs * 2
-        self.prob_xover = prob_xover
-        self.prob_mut = prob_mut
+        self.prob_xover = prob_xover   # Probability of performing crossover of two parents to create a child during iterations
+        self.prob_mut = prob_mut  # Probability of mutating a child during iterations
         self.fitness = fitness
         self.population = []
         self.children = []
@@ -93,6 +96,7 @@ class GA(object):
         self.average.append(sum(self.curr_scores)/self.pop_size)
         self.iter_index = 0
 
+    # Roulette selector for use during iterations
     def roulette(self, unif: float) -> int:
         counter = 0
         total = sum(self.curr_scores)
@@ -114,6 +118,7 @@ class GA(object):
 
     def _iterate(self):
         self.children_scores = []
+        # Create random numbers 0.0-1.0 used to determine if crossover will occur and if each individual child will be mutated
         tmp_prob_xover = rd.random()
         tmp_prob_mut1 = rd.random()
         tmp_prob_mut2 = rd.random()
@@ -142,17 +147,17 @@ class GA(object):
             if tmp_prob_mut1 < self.prob_mut:
                 rnd = rd.choice([1, 2, 3])
                 match rnd:
-                    case 1:
+                    case 1:  # Mutation 1: Swap two full columns in a child
                         sample = rd.sample(list(range(0, self.dims)), 2)
                         self.children[-2]._swap_cols(sample[0], sample[1])
                         self.children[-2].fitness = self.fitness(self.children[-2])
                         self.children_scores[-2] = self.children[-2].fitness
-                    case 2:
+                    case 2:  # Mutation 2: Swap two full rows in a child
                         sample = rd.sample(list(range(0, self.dims)), 2)
                         self.children[-2]._swap_rows(sample[0], sample[1])
                         self.children[-2].fitness = self.fitness(self.children[-2])
                         self.children_scores[-2] = self.children[-2].fitness
-                    case 3:
+                    case 3:  # Mutation 3: Swap any two individual cells in a child
                         sample1 = rd.sample(list(range(0, self.dims)), 2)
                         sample2 = rd.sample(list(range(0, self.dims)), 2)
                         self.children[-2]._swap_elems(sample1[0], sample1[1], sample2[0], sample2[1])
@@ -183,7 +188,7 @@ class GA(object):
         self.cumul_scores = []
         curr_sum = 0
         counter = 0
-        while counter < self.pop_size:
+        while counter < self.pop_size:  # Process the full population at each iteration
             self.population.append(next_generation[counter][1])
             self.curr_scores.append(self.population[-1].fitness)
             curr_sum = sum(self.curr_scores)
@@ -203,7 +208,7 @@ class GA(object):
 
 
 if __name__ == '__main__':
-    a = GA(8, cr.DatType.INTEGER, 0.0, 1.0, 5, 0.9, 0.1, fit_func)
+    a = GA(10, cr.DatType.INTEGER, 0.0, 1.0, 5, 0.9, 0.1, fit_func)
     print(a.history, a.minimum, a.maximum, a.average)
     a.run(10, stagnancy=100)
 
@@ -213,7 +218,8 @@ if __name__ == '__main__':
     print(a.average)
 
     for chromo in a.population:
-        print(chromo, "\n", chromo.fitness, "\n\n")
+        chromo_string = chromo.get_string()
+        print(f"({chromo_string})\n{chromo}\nFitness Score (F1): {chromo.fitness}\n\n")
 
     # Data for plotting
     t = np.arange(len(a.average))
@@ -223,7 +229,7 @@ if __name__ == '__main__':
     ax.plot(t, s)
 
     ax.set(xlabel='Iterations', ylabel='Fitness Function',
-           title='Average F1 Score Over Iterations (8-Qubit UNSW_NB15)')
+           title='Average F1 Score Over Iterations (10-Qubit IoT23)')
     ax.grid()
     # plt.show()
     plt.savefig("Genalg.png")
